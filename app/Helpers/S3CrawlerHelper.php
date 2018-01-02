772afb49429688f50d01a39752f5a71dd31344e9
Nick T-bone Kiermaier <nickkiermaier@gmail.com>
2018-01-01 22:16:03 -0600

    upd hosts

--- app/Helpers/S3CrawlerHelper.php
+++ app/Helpers/S3CrawlerHelper.php
@@ -10,11 +10,23 @@ class S3CrawlerHelper
      * @var array
      */
     public $hosts = [
-        'http://s3-eu-west-1.amazonaws.com',
+        'http://s3-us-east-2.amazonaws.com',
+        'http://s3-external-1.amazonaws.com',
         'http://s3-us-west-1.amazonaws.com',
-        'http://s3.amazonaws.com',
+        'http://s3-us-west-2.amazonaws.com',
+        'http://s3-ca-central-1.amazonaws.com',
+        'http://s3-ap-south-1.amazonaws.com',
+        'http://s3-ap-northeast-2.amazonaws.com',
         'http://s3-ap-southeast-1.amazonaws.com',
-        'http://s3-ap-northeast-1.amazonaws.com'
+        'http://s3-ap-southeast-2.amazonaws.com',
+        'http://s3-ap-northeast-1.amazonaws.com',
+        'http://s3.cn-north-1.amazonaws.com.cn',
+        'http://s3.cn-northwest-1.amazonaws.com.cn',
+        'http://s3-eu-central-1.amazonaws.com',
+        'http://s3-eu-west-1.amazonaws.com',
+        'http://s3-eu-west-2.amazonaws.com',
+        'http://s3-eu-west-3.amazonaws.com',
+        'http://s3-sa-east-1.amazonaws.com'
     ];
 
     /**
@@ -25,6 +37,12 @@ class S3CrawlerHelper
     {
         $urls = $this->buildUrls($word);
         $results = HttpHelper::asyncGet($urls);
+
+        foreach ($results as $key => $result) {
+
+        }
+
+
         return $results;
     }
 
@@ -50,6 +68,11 @@ class S3CrawlerHelper
 
     public function isSuccessUrl($response)
     {
+        $response['state'] = "fulfilled";
+        $response_body = (string)$response['value']->getBody();
+
 
     }
+
+
 }
