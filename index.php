7fd5e7884af19db8fa040186e57af95e38079359
Nick T-bone Kiermaier <nickkiermaier@gmail.com>
2018-01-03 14:06:31 -0600

    upd

--- index.php
+++ index.php
@@ -3,42 +3,41 @@
 require __DIR__ . '/vendor/autoload.php';
 use Mutant\S3Crawler\App\Helpers\S3CrawlerHelper;
 use Mutant\File\App\Helpers\File;
-use Mutant\File\App\Helpers\FileHelper;
 
 $s3crawler = new S3CrawlerHelper();
 
-// Create a directory for the output files
-FileHelper::createDirectory("./s3files");
-
-
 $input_filename = $argv[1]; // get filename for file of names as command line argument
 $input_file = new File($input_filename); // open that file
-$fail_file = new File("./s3crawlerfail.txt");
+$success_file = new File("./s3crawlersuccess.txt");
+
+
+// Determines whether to create error file and keep failed requests
+if ($argv[2] == 'keep_errs') {
+    $keep_errs = true;
+    $fail_file = new File("./s3crawlerfail.txt");
+}
+
 
 while (!$input_file->eof()) {
-    $next_name = $input_file->getLineDataAndAdvance();
-    $results = $s3crawler->run($next_name);
+    $next_name = $input_file->getLineDataAndAdvance(); //get next name from input file
+    $results = $s3crawler->run($next_name); // run the crawler
 
-    foreach ($results as $key => $result) {
-        $word = $result['S3CrawlerWord'];
-        if ($result['S3CrawlerStatus'] == 'success') {
-            $filename = "./s3files/$word.txt";
-            $success_file = new File($filename);
+    // process results
+    foreach ($results as $result) {
 
+        if ($result->status == 'success') {
+            $success_file->appendAndStay($result->bucketname);
+            continue;
         }
 
-        if ($result['S3CrawlerStatus'] == 'fail' || $result['S3CrawlerStatus'] == 'undetermined') {
-            $errorcode = $result['S3CrawlerErrorCode'];
+        if ($keep_errs) {
             $out = [
-                "word" => $word,
-                "bucket" => $key,
-                "errorcode" => $errorcode
+                "bucketname" => $result->bucketname,
+                "error" => $result->error_messages
             ];
 
             $out = json_encode($out);
             $fail_file->appendAndStay($out);
-
         }
     }
-
 }
