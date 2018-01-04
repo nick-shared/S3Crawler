acb223da567c0ebe2f00854f9a1d310c3ce189a9
Nick T-bone Kiermaier <nickkiermaier@gmail.com>
2018-01-03 19:26:12 -0600

    upd

--- database/migrations/2018_01_03_214432_create_s3failbuckets_table.php
+++ database/migrations/2018_01_03_214432_create_s3failbuckets_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateS3openbucketsTable extends Migration
+class CreateS3failbucketsTable extends Migration
 {
     /**
      * Run the migrations.
